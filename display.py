6b1f99e51f1440c78ba20564494f6f3a656bb4b5
baffoRti <saha14373@gmail.com>
2026-03-18 15:38:09 +0300

    Edit display

--- display.py
+++ display.py
@@ -26,18 +26,6 @@ def display_db_contents(db_path):
         print("No records found in user_traffic_monthly.")
 
 
-    print()
-    cursor.execute("SELECT * FROM log_metadata")
-    log_metadata_records = cursor.fetchall()
-    if log_metadata_records:
-        print("Last Updated Time")
-        print("--------------------")
-        for row in log_metadata_records:
-            print(f"{row[1]}")
-    else:
-        print("No records found in log_metadata.")
-
-
     print("\n--- Current client state ---")
     cursor.execute("SELECT * FROM current_client_state")
     current_client_state_records = cursor.fetchall()
@@ -55,6 +43,18 @@ def display_db_contents(db_path):
             print(f"{common_name:<15} | {connected_since:<19} | {human_received:<16} | {human_sent:<12} | {human_total:<12}")
     else:
         print("No records found in current_client_state.")
+
+    print()
+    cursor.execute("SELECT * FROM log_metadata")
+    log_metadata_records = cursor.fetchall()
+    if log_metadata_records:
+        print("Last Updated Time")
+        print("--------------------")
+        for row in log_metadata_records:
+            print(f"{row[1]}")
+    else:
+        print("No records found in log_metadata.")
+
 
     # Display overall monthly traffic summary
     print("\n--- Overall Monthly Traffic Summary ---")
