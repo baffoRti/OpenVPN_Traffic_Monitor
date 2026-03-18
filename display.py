9d302adae028ad4ba973cc7f42e17d52d6b5a1bf
baffoRti <saha14373@gmail.com>
2026-03-18 15:02:51 +0300

    Edit display

--- display.py
+++ display.py
@@ -1,7 +1,6 @@
 import sqlite3
-import config  # For openvpn_stats_db
+import config # For openvpn_stats_db
 import utils  # For convert_bytes_to_human_readable
-
 
 def display_db_contents(db_path):
     conn = sqlite3.connect(db_path)
@@ -11,15 +10,20 @@ def display_db_contents(db_path):
     cursor.execute("SELECT * FROM user_traffic_monthly")
     user_traffic_records = cursor.fetchall()
     if user_traffic_records:
-        print("Common Name     | Year-Month | Bytes Received | Bytes Sent")
-        print("------------------------------------------------------------")
+        # Sort records alphabetically by common_name
+        user_traffic_records.sort(key=lambda x: x[0].lower())
+        print("Common Name | Year-Month | Received Traffic | Sent Traffic | Total Traffic")
+        print("----------------------------------------------------------------------------")
         for row in user_traffic_records:
             common_name, year_month, bytes_received, bytes_sent = row
+            total_traffic_bytes = bytes_received + bytes_sent
             human_received = utils.convert_bytes_to_human_readable(bytes_received)
             human_sent = utils.convert_bytes_to_human_readable(bytes_sent)
-            print(f"{common_name:<15} | {year_month:<10} | {human_received:<14} | {human_sent:<10}")
+            human_total = utils.convert_bytes_to_human_readable(total_traffic_bytes)
+            print(f"{common_name:<15} | {year_month:<10} | {human_received:<18} | {human_sent:<12} | {human_total:<13}")
     else:
         print("No records found in user_traffic_monthly.")
+
 
     print("\n--- Contents of log_metadata table ---")
     cursor.execute("SELECT * FROM log_metadata")
@@ -32,26 +36,30 @@ def display_db_contents(db_path):
     else:
         print("No records found in log_metadata.")
 
+
     print("\n--- Contents of current_client_state table ---")
     cursor.execute("SELECT * FROM current_client_state")
     current_client_state_records = cursor.fetchall()
     if current_client_state_records:
-        print("Common Name     | Connected Since     | Bytes Received | Bytes Sent")
-        print("---------------------------------------------------------------------")
+        # Sort records alphabetically by common_name
+        current_client_state_records.sort(key=lambda x: x[0].lower())
+        print("Common Name | Connected Since   | Received Traffic | Sent Traffic | Total Traffic")
+        print("-----------------------------------------------------------------------------------")
         for row in current_client_state_records:
             common_name, connected_since, bytes_received, bytes_sent = row
+            total_traffic_bytes = bytes_received + bytes_sent
             human_received = utils.convert_bytes_to_human_readable(bytes_received)
             human_sent = utils.convert_bytes_to_human_readable(bytes_sent)
-            print(f"{common_name:<15} | {connected_since:<19} | {human_received:<14} | {human_sent:<10}")
+            human_total = utils.convert_bytes_to_human_readable(total_traffic_bytes)
+            print(f"{common_name:<15} | {connected_since:<19} | {human_received:<18} | {human_sent:<12} | {human_total:<13}")
     else:
         print("No records found in current_client_state.")
 
     conn.close()
-
 
 if __name__ == '__main__':
     # This allows direct execution of display.py for debugging or standalone use.
     # It will connect to the default database defined in config.py.
     print("\n--- Displaying database contents (standalone) ---")
     display_db_contents(config.OPENVPN_STATS_DB)
-    print("--- Display complete ---")
+    print("--- Display complete ---\n")
